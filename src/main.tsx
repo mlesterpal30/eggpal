fd2a5c0c3923951ded4e5d461845a4c1846bc933
mlesterpal30 <mlestermarananpal@gmail.com>
2025-12-31 17:13:10 +0800

    Add Chakra UI and refactor styling components
    Updated `package.json` to include new dependencies:
    `@chakra-ui/react`, `@emotion/react`, `@emotion/styled`,
    and `framer-motion`. Removed legacy styles from
    `App.css` and `index.css`, transitioning to Chakra UI
    for styling. Refactored `App.tsx` to utilize Chakra
    components, and wrapped the application in
    `ChakraProvider` in `main.tsx`. Updated
    `package-lock.json` to reflect these changes.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const fonts = {
+    'geist': "'Geist', sans-serif",
+}
+
+const theme = extendTheme({ fonts })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
+    <React.StrictMode>
+        <ChakraProvider theme={theme }>
+            <App />
+        </ChakraProvider>
   </React.StrictMode>,
 )
